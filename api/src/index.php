cd0bb44b83a835063a62bf5ca981bd991c75240a
pandaguerrier <juleslofficial@gmail.com>
2026-02-17 10:55:59 +0100

    feat: enhance random string generation and add health check endpoint

--- api/src/index.php
+++ api/src/index.php
@@ -16,19 +16,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     exit();
 }
 
-header("Access-Control-Allow-Origin: http://127.0.0.1:8080");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
-
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
-
 $router = new \Bramus\Router\Router();
 
 $router->get('/', function() {
     echo "Bienvenue sur l'API ParmClicker !";
+    exit();
+});
+
+$router->get('/v1/health', function() {
+    header('Content-Type: application/json');
+    echo json_encode(['status' => 'ok']);
 });
 
 $router->get('/users', function() { $users = UserModel::getAll(); header('Content-Type: application/json'); echo json_encode($users); });
@@ -52,6 +49,7 @@ $router->post('/login', function() {
     //echo json_encode(login($db));
     login($db);
 });
+
 
 
 $router->run();
